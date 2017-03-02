2608bcc857369eda1742ead63e48b7dbbe112a94
Linus Lewandowski <linus@lew21.net>
2017-03-02 23:10:21 +0100

    Replace spaces with tabs.

--- django_auth_oidc/urls.py
+++ django_auth_oidc/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'django_auth_oidc'
 urlpatterns = [
-    url(r'^$', views.login, name='login'),
-    url(r'^callback/$', views.callback, name='callback'),
+	url(r'^$', views.login, name='login'),
+	url(r'^callback/$', views.callback, name='callback'),
 ]
--- django_auth_oidc/views.py
+++ django_auth_oidc/views.py
@@ -7,31 +7,31 @@ from django.utils.http import is_safe_url
 from . import oidc
 
 def login(request):
-    return_path = request.GET.get(auth.REDIRECT_FIELD_NAME, "")
+	return_path = request.GET.get(auth.REDIRECT_FIELD_NAME, "")
 
-    return redirect(oidc.server.authorize(
-        redirect_uri = request.build_absolute_uri(reverse("django_auth_oidc:callback")),
-        state = return_path,
-    ))
+	return redirect(oidc.server.authorize(
+		redirect_uri = request.build_absolute_uri(reverse("django_auth_oidc:callback")),
+		state = return_path,
+	))
 
 def callback(request):
-    return_path = request.GET.get("state")
+	return_path = request.GET.get("state")
 
-    res = oidc.server.request_token(
-        redirect_uri = request.build_absolute_uri(reverse("django_auth_oidc:callback")),
-        code = request.GET["code"],
-    )
+	res = oidc.server.request_token(
+		redirect_uri = request.build_absolute_uri(reverse("django_auth_oidc:callback")),
+		code = request.GET["code"],
+	)
 
-    User = auth.get_user_model()
-    user, created = User.objects.get_or_create(username=res.id["sub"])
-    auth.login(request, user)
+	User = auth.get_user_model()
+	user, created = User.objects.get_or_create(username=res.id["sub"])
+	auth.login(request, user)
 
-    url_is_safe = is_safe_url(
-        url = return_path,
-        host = request.get_host(),
-        #allowed_hosts = set(request.get_host()),
-        #require_https = request.is_secure(),
-    )
-    if not url_is_safe:
-        return redirect(resolve_url(settings.LOGIN_REDIRECT_URL))
-    return redirect(return_path)
+	url_is_safe = is_safe_url(
+		url = return_path,
+		host = request.get_host(),
+		#allowed_hosts = set(request.get_host()),
+		#require_https = request.is_secure(),
+	)
+	if not url_is_safe:
+		return redirect(resolve_url(settings.LOGIN_REDIRECT_URL))
+	return redirect(return_path)
