79e8f5901b9e57bcbe4cf6694b7ad7a7b9012ad2
Linus Lewandowski <linus@lew21.net>
2017-06-10 19:16:35 +0200

    Fix get_user_by_username().

--- django_auth_oidc/__init__.py
+++ django_auth_oidc/__init__.py
@@ -2,6 +2,6 @@ from django.contrib.auth import get_user_model
 
 
 def get_user_by_username(id):
-	User = auth.get_user_model()
+	User = get_user_model()
 	user, created = User.objects.get_or_create(username=id["sub"])
 	return user
