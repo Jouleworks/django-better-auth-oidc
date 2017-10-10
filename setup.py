1b1dd0a41d355dba8c44a153e41626b798239f97
Linus Lewandowski <llewandowski@opera.com>
2017-10-10 12:46:29 +0200

    Release 0.4.3.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
 	name = "django-auth-oidc",
-	version = "0.4.2",
+	version = "0.4.3",
 	description = "OpenID Connect authentication support for Django",
 	long_description = readme,
 	author = "Aiakos Contributors",
